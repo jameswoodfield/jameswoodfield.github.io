70857dfcae6be6c9409abdef53a1f8d404e949f0
james <James14641@gmail.com>
2025-09-17 08:46:20 +0100

    Sort portfolio files alphabetically in parser
    Changed the sorting of portfolio markdown files in parse_portfolio from numeric order based on filename numbers to simple alphabetical order. This simplifies the sorting logic and may affect the order in which portfolio items are processed.

--- scripts/cv_markdown_to_json.py
+++ scripts/cv_markdown_to_json.py
@@ -344,7 +344,8 @@ def parse_portfolio(portfolio_dir):
     
     portfolio_files = glob.glob(os.path.join(portfolio_dir, "*.md"))
     # sort portfolio file by number in filename
-    sorted_portfolio_files = sorted(portfolio_files, key=lambda x: int(re.search(r'(\d+)', os.path.basename(x)).group(1)) if re.search(r'(\d+)', os.path.basename(x)) else float('inf'))
+    #sorted_portfolio_files = sorted(portfolio_files, key=lambda x: int(re.search(r'(\d+)', os.path.basename(x)).group(1)) if re.search(r'(\d+)', os.path.basename(x)) else float('inf'))
+    sorted_portfolio_files = sorted(portfolio_files)
     for portfolio_file in sorted_portfolio_files:
         with open(portfolio_file, 'r', encoding='utf-8') as file:
             content = file.read()
